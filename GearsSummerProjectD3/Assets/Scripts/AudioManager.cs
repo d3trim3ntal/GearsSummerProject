141f08347e1278acefd1d845b09cbe33c96ed3b5
d3trim3ntal <obsidianblackrose@gmail.com>
2023-08-08 23:05:37 -0700

    Updated Main Menu
    Volume Slider works
    Main Menu script and Audio script added
    All buttons work
    Need to finish up customization and settings to start actual game.

---
+++ GearsSummerProjectD3/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
